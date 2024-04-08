3ec7f6c986e09607c285fe5ca0c3c72a20cad376
Keks <keksrespekt@gmail.com>
2024-04-08 20:10:48 +0300

    Добавил задачу C#

---
+++ Задача C#/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("Введите элементы массива через пробел:");
+string input = Console.ReadLine()!;
+
+string[] inputArray = input.Split(' ');
+
+int count = 0;
+foreach (string str in inputArray)
+{
+    if (str.Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] resultArray = new string[count];
+
+int index = 0;
+foreach (string str in inputArray)
+{
+    if (str.Length <= 3)
+    {
+        resultArray[index++] = str;
+    }
+}
+Console.WriteLine("Результат:");
+Console.Write("[");
+for (int i = 0; i < resultArray.Length; i++)
+{
+    Console.Write(resultArray[i]);
+    if (i < resultArray.Length - 1)
+    {
+        Console.Write(" ");
+    }
+}
+Console.Write("]");
